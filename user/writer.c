e18de2f48e685a4c91649b2617c3e15e265ffbc9
Djordje Zivanovic <djolezile@gmail.com>
2022-06-05 19:28:36 +0200

    writer implemented

---
+++ user/writer.c
@@ -0,0 +1,25 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+int
+main(int argc, char *argv[])
+{
+	int fd, bytes;
+	char* a;
+
+	if (argc == 3){
+		open(argv[1], O_CREATE);	
+		fd = open(argv[1], O_WRONLY);
+		bytes = atoi(argv[2]);
+		a = (char*)malloc(bytes);
+		memset(a, 'a', bytes);
+		write(fd, a, bytes);
+		exit();	
+	} else {
+		printf("writer: bad request: 'writer -filename -bytes'\n");
+		exit();
+	}
+}
